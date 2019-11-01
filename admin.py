3116635d710a15439eaa5716faa97cf0f81319a1
Goktug <gokktugbasaran@gmail.com>
2019-11-01 12:21:19 +0300

    Revoke admin auth for dev. purposes

--- admin.py
+++ admin.py
@@ -6,29 +6,25 @@ sqlpage = Blueprint(name='sqlpage', import_name=__name__)
 
 @sqlpage.route("/admin_sql", methods=['GET', 'POST'])
 def sql_page():
-	if session.get('member_id') != 'admin':
-		flash("Authorization error",'danger')
-		return redirect(url_for('home.home_page'))
-	else:
-		form = SQLForm()
-		if form.validate_on_submit():
-			query = form.query.data
-			print("Running")
-			print(query)
-			result = None
-			resLen = 0
-			try:
-				connection = db.connect(os.getenv("DATABASE_URL"))
-				cursor = connection.cursor()
-				cursor.execute(query)
-				result = cursor.fetchall()
-				resLen = len(result)
-			except db.DatabaseError as dberror:
-				connection.rollback()
-				result = dberror
-				resLen = 1
-				flash('Query unsuccessful.', 'danger')
-			finally:
-				connection.close()
-				return render_template('sql_page.html', form=form,queryResult=result,resultLen=resLen)
-		return render_template('sql_page.html',form=form,error=error,queryResult=None,resultLen=0)
+	form = SQLForm()
+	if form.validate_on_submit():
+		query = form.query.data
+		print("Running")
+		print(query)
+		result = None
+		resLen = 0
+		try:
+			connection = db.connect(os.getenv("DATABASE_URL"))
+			cursor = connection.cursor()
+			cursor.execute(query)
+			result = cursor.fetchall()
+			resLen = len(result)
+		except db.DatabaseError as dberror:
+			connection.rollback()
+			result = dberror
+			resLen = 1
+			flash('Query unsuccessful.', 'danger')
+		finally:
+			connection.close()
+			return render_template('sql_page.html', form=form,queryResult=result,resultLen=resLen)
+	return render_template('sql_page.html',form=form,error=error,queryResult=None,resultLen=0)
