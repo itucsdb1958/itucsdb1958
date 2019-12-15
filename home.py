d3aeabe8a22a550bf6c063f911f12fd41fdaced4
Farid <hferid23@gmail.com>
2019-12-15 16:32:21 +0300

    List all the teams on the home page

--- home.py
+++ home.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, render_template, redirect, url_for, flash, request, session,abort,Blueprint
 import psycopg2 as db
+from forms import *
+from queries import select
 
-home= Blueprint(name='home', import_name=__name__)
+home= Blueprint(name='home', import_name=__name__,
+                    template_folder='templates')
 
 @home.route("/")
 def home_page():
-	return render_template('home_page.html')
+    teams = select(columns="team.name,competition.name,team.email,team.adress,team.id,team.logo",
+                   table="team join competition on team.competition_id=competition.id order by team.name desc")
+    return render_template("home_page.html", teams=teams)
